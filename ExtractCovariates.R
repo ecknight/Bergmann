663c8bb108506949d0343bafa857cf7f51c163d1
ecknight <ellyknight@gmail.com>
2022-03-16 12:02:55 -0600

    Pulling many covs

--- ExtractCovariates.R
+++ ExtractCovariates.R
@@ -7,9 +7,6 @@ library(lubridate)
 library(data.table)
 library(usdm)
 
-#CHANGE ME!!!!!
-setwd("/Users/ellyknight/Documents/UoA/Projects/Projects/Morphometrics/")
-
 #1. Initialize rgee----
 #ee_install()
 ee_check()
@@ -37,10 +34,33 @@ buffer_points <- function(feature){
 }
 
 #3. Import data----
-dat <- read.csv("Bergs_rule.csv") %>% 
-  dplyr::select(TagID, Banding.Date, B.Lat, B.Long) %>% 
-  rename(X = B.Lat, Y = B.Long, Date = Banding.Date) %>% 
-  mutate(Date = mdy(Date))
+dat.raw <- read.csv("capri.locs.csv") %>% 
+  dplyr::filter(!is.na(B.Lat),
+                !is.na(W.Lat)) %>% 
+  mutate(ID = row_number())
+
+write.csv(dat.raw, "capri.locs.bw.csv", row.names = FALSE)
+
+dat.b <- dat.raw %>% 
+  mutate(ID = row_number()) %>% 
+  rename(Y = B.Lat, X = B.Long, Date = Banding.Date) %>% 
+  mutate(Date = mdy(Date),
+         season = "Breed") %>% 
+  dplyr::select(ID, Species, Date, X, Y, season)
+
+dat.w <- dat.raw %>% 
+  mutate(ID = row_number()) %>% 
+  rename(Y = W.Lat, X = W.Long) %>% 
+  mutate(Date = mdy(paste0("12-15-", Year)),
+         season = "Winter") %>% 
+  dplyr::select(ID, Species, Date, X, Y, season)
+
+dat <- rbind(dat.b, dat.w)
+
+#4. Visualize----
+ggplot(dat) +
+  geom_point(aes(x=X, y=Y, colour=Species)) +
+  facet_wrap(~season)
 
 #4. Wrangle data----
 dat$Date <- as.POSIXct(dat$Date, format = "%Y-%m-%d %H:%M:%S", tz="UTC") 
@@ -54,41 +74,134 @@ datasf <- st_as_sf(dat, coords = c('X','Y'), crs = 4326)
 #6. Send data to GEE----
 data <- sf_as_ee(datasf)
 
-#7. Load EVI image collection----
-imagecoll <- ee$ImageCollection('JAXA/ALOS/AW3D30/V3_2')
-dsm <- imagecoll$select('DSM')
+#7. EVI----
+evi <- ee$ImageCollection('LANDSAT/LC08/C01/T1_8DAY_EVI')
 
-#8. Extract EVI point values----
 data.evi <- ee_extract(
-  x = dsm,
+  x = evi,
   y = datasf,
   scale = 30,
   sf = FALSE
 )
+data.evi.wide <- left_join(dat, data.evi)
 
-#9. Load landcover image----
-lc <- ee$Image('COPERNICUS/Landcover/100m/Proba-V-C3/Global/2017')
+data.evi.long <- data.evi.wide %>% 
+  pivot_longer(8:ncol(data.evi.wide), names_to="layer", values_to="value") %>% 
+  mutate(covdate = str_sub(layer, 1, 9),
+         cov = str_sub(layer, 11, 100)) %>% 
+  dplyr::select(-layer) %>% 
+  pivot_wider(names_from="cov", values_from="value") %>% 
+  mutate(covyear = as.numeric(str_sub(covdate, 2, 5)),
+         covmonth = as.numeric(str_sub(covdate, 6, 7)),
+         covday = as.numeric(str_sub(covdate, 8, 9)))
 
-#10. Extract landcover point values----
-data.lc <- ee_extract(
-  x = lc,
+write.csv(data.evi.long, "EVI.csv", row.names = FALSE)
+
+#8. DEM----
+dem <- ee$Image('USGS/GTOPO30')
+
+data.dem <- ee_extract(
+  x = dem,
   y = datasf,
-  scale = 100,
+  sf = FALSE
+)
+write.csv(data.dem, "DEM.csv", row.names = FALSE)
+
+#9. Worldclim monthly----
+clim <- ee$ImageCollection('WORLDCLIM/V1/MONTHLY')
+
+data.clim <- ee_extract(
+  x = clim,
+  y = datasf,
   sf = FALSE
 )
 
-#11. Buffer points----
-data.buff <- data$map(buffer_points)
+data.clim.wide <- left_join(dat, data.clim)
 
-#12. Extract buffer mean lc values----
-lc.buff <- lc$reduceRegions(collection=data.buff, 
-                            reducer=ee$Reducer$mean(), 
-                            scale=30)
+data.clim.long <- data.clim.wide %>% 
+  pivot_longer(8:ncol(data.clim.wide), names_to="layer", values_to="value") %>% 
+  mutate(covdate = str_sub(layer, 1, 3),
+         cov = str_sub(layer, 5, 100)) %>% 
+  dplyr::select(-layer) %>% 
+  pivot_wider(names_from="cov", values_from="value") %>% 
+  mutate(covmonth = as.numeric(str_sub(covdate, 2, 3)))
 
-#13. Export lc task to google drive----
-task_vector <- ee_table_to_drive(collection=lc.buff,
-                                 description=paste0("LC_test_"),
-                                 folder="Test",
-                                 timePref=FALSE)
-task_vector$start()
-ee_monitoring(task_vector) # optional
+
+
+write.csv(data.clim, "Wordclim.csv", row.names = FALSE)
+
+#10. ERA monthly----
+years <- seq(1981, 2021, 1) 
+
+data.era <- list()
+for(i in 1:length(years)){
+  
+  start<-paste0(years[i], "-01-01")
+  end<-paste0(years[i],"-12-31")
+  
+  era<-ee$ImageCollection("ECMWF/ERA5_LAND/MONTHLY")$filterDate(start,end)
+  
+  data.i <- ee_extract(
+    x = era,
+    y = datasf,
+    sf = FALSE
+  )
+  
+  data.era[[i]] <- data.i %>% 
+    mutate(year = years[i])
+  
+  print(paste0("Completed year ", years[i], " of ", length(years), " years"))
+}
+
+data.era.wide <- cbind(dat,
+                       do.call(cbind, data.era) %>% 
+  dplyr::select(-ID, -Species, -Date, -season, -year))
+
+data.era.long <- data.era.wide %>% 
+  pivot_longer(8:ncol(data.era.wide), names_to="layer", values_to="value") %>% 
+  mutate(covdate = str_sub(layer, 1, 7),
+         cov = str_sub(layer, 9, 100)) %>% 
+  dplyr::select(-layer) %>% 
+  pivot_wider(names_from="cov", values_from="value") %>% 
+  mutate(covyear = as.numeric(str_sub(covdate, 2, 5)),
+         covmonth = as.numeric(str_sub(covdate, 6, 7)))
+
+write.csv(data.era.long, "ERA5.csv", row.names = FALSE)
+
+#11. Terraclim monthly----
+years <- seq(1958, 2020, 1) 
+
+data.terra <- list()
+for(i in 1:length(years)){
+  
+  start<-paste0(years[i], "-01-01")
+  end<-paste0(years[i],"-12-31")
+  
+  terra<-ee$ImageCollection("IDAHO_EPSCOR/TERRACLIMATE")$filterDate(start,end)
+  
+  data.i <- ee_extract(
+    x = terra,
+    y = datasf,
+    sf = FALSE
+  )
+  
+  data.terra[[i]] <- data.i %>% 
+    mutate(year = years[i])
+  
+  print(paste0("Completed year ", years[i], " of ", length(years), " years"))
+}
+
+data.terra.wide <- cbind(dat,
+                       do.call(cbind, data.terra) %>% 
+                         dplyr::select(-ID, -Species, -Date, -season, -year))
+
+data.terra.long <- data.terra.wide %>% 
+  pivot_longer(8:ncol(data.terra.wide), names_to="layer", values_to="value") %>% 
+  mutate(covdate = str_sub(layer, 1, 7),
+         cov = str_sub(layer, 9, 100)) %>% 
+  dplyr::select(-layer) %>% 
+  pivot_wider(names_from="cov", values_from="value") %>% 
+  mutate(covyear = as.numeric(str_sub(covdate, 2, 5)),
+         covmonth = as.numeric(str_sub(covdate, 6, 7)))
+
+write.csv(data.terra.long, "Terraclim.csv", row.names = FALSE)
